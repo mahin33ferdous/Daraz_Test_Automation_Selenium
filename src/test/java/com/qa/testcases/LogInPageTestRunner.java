21caa57839ba4b86b8d134823c07242df06d2958
mahin33ferdous <mahinferdousshoily@gmail.com>
2024-03-15 23:22:04 -1100

    updated changes

--- src/test/java/com/qa/testcases/LogInPageTestRunner.java
+++ src/test/java/com/qa/testcases/LogInPageTestRunner.java
@@ -9,6 +9,13 @@ import com.qa.pages.LogInPage;
 
 public class LogInPageTestRunner extends BaseClass {
 	 LogInPage loginPage;
+	 
+	 public void checkNotLoggedIn() {
+		 
+			boolean Validate_logIn = driver.findElement(By.xpath("//span[@id='myAccountTrigger']")).isDisplayed();
+ 		    Assert.assertFalse(Validate_logIn );
+		 
+	 }
 
 		
 		// get url of targeted website
@@ -21,34 +28,37 @@ public class LogInPageTestRunner extends BaseClass {
 		
 //.....................................................Normal Login................................................................................
 		
-		@Test(priority=1, description = "Validate Normal Login")
+		@Test(priority=1, description = "User will be able to logged in normally with correct email and password")
 		public void validNormalLogin() throws InterruptedException {
 			
 			loginPage=new LogInPage(driver);
 			loginPage.login("shoily9192@gmail.com", "daraz91922");
 			boolean Validate_logIn = driver.findElement(By.xpath("//span[@id='myAccountTrigger']")).isDisplayed();
-			Assert.assertTrue(Validate_logIn );
+			Assert.assertTrue(Validate_logIn );     // check user logged in successfully......................................
 			loginPage.doLogOut();
 			}
 		
 		
 //.....................................................Empty Email................................................................................		
 		
-		@Test(priority=6, description = "Validate error with empty Email")
+		@Test(priority=2, description = "Varify error text with empty Email and user will be not able to log in")
 		public void validEmailTest1() throws InterruptedException {
 			
 			loginPage=new LogInPage(driver);
 			//loginPage.passwordValidation("", "daraz91922");
 			loginPage.login("", "daraz91922");
-	        String validationError_Email_actual = driver.findElement(By.xpath("//form[1]/div[1]/div[1]/div[2]/span[1]")).getText();
+	        String validationError_Email_actual = driver.findElement(By.xpath("//form[1]/div[1]/div[1]/div[1]/span[1]")).getText();
 	        String validationError_expected = "You can't leave this empty.";
-	        Assert.assertFalse(validationError_Email_actual.contains(validationError_expected));
+	        //Assert.assertFalse(validationError_Email_actual.contains(validationError_expected));
+	        Assert.assertTrue(validationError_Email_actual.contains(validationError_expected)); // check error message is displayed
+	        
+	        checkNotLoggedIn(); // User is unable to log in......................................
 		}
 		
 		
 //.....................................................asd........................................................................................			
 		
-		@Test(priority=3, description = "Validate invalid email error with asd")
+		@Test(priority=3, description = "Varify error text with empty email \"asd\" and user will be not able to log in")
 		public void validEmailTest2() throws InterruptedException {
 			
 			loginPage=new LogInPage(driver);
@@ -56,14 +66,16 @@ public class LogInPageTestRunner extends BaseClass {
 			loginPage.login("asd", "daraz91922");
 	        String validationError_actual = driver.findElement(By.xpath("//form[1]/div[1]/div[1]/div[1]/span[1]")).getText();
 	        String validationError_expected = "The length of the Phone or Email should be 6-60 characters.";
-	        Assert.assertTrue(validationError_actual.contains(validationError_expected));
+	        Assert.assertTrue(validationError_actual.contains(validationError_expected)); // check error message is displayed
+	        
+	        checkNotLoggedIn(); // User is unable to log in......................................
 		}
 		
 		
 		
 //.....................................................asd@........................................................................................			
 		
-		@Test(priority=3, description = "Validate invalid email error with asd@")
+		@Test(priority=4, description = "Varify error text with empty email \\\"asd\\\" and user will be not able to log in")
 		public void validEmailTest3() throws InterruptedException {
 			
 			loginPage=new LogInPage(driver);
@@ -71,49 +83,58 @@ public class LogInPageTestRunner extends BaseClass {
 			loginPage.login("asd@", "daraz91922");
 	        String validationError_actual = driver.findElement(By.xpath("//form[1]/div[1]/div[1]/div[1]/span[1]")).getText();
 	        String validationError_expected = "The length of the Phone or Email should be 6-60 characters.";
-	        Assert.assertTrue(validationError_actual.contains(validationError_expected));
+	        Assert.assertTrue(validationError_actual.contains(validationError_expected)); // check error message is displayed
+	        
+	        checkNotLoggedIn(); // User is unable to log in......................................
 		
 		}
 		
 		
 //....................................................asd@gmail.com........................................................................................			
 		
-		@Test(priority=3, description = "Validate invalid email error text with asd@gmail.com")
+		@Test(priority=5, description = "Varify error text with empty email \\\"asd@gmail.com\\\" and user will be not able to log in. ")
 		public void validEmailTest4() throws InterruptedException {
 			
 			loginPage=new LogInPage(driver);
 			//loginPage.emailValidation("asd");
 			loginPage.login("asd@gmail.com", "daraz91922");
 			boolean Error_Validate_Invalid_Email = driver.findElement(By.xpath("//form[1]/div[1]/div[1]/div[1]/span[1]")).isDisplayed();
-			Assert.assertFalse(Error_Validate_Invalid_Email);
+			Assert.assertFalse(Error_Validate_Invalid_Email); // check error message is displayed
 			
-			boolean Validate_logIn = driver.findElement(By.xpath("//span[@id='myAccountTrigger']")).isDisplayed();
- 		    Assert.assertFalse(Validate_logIn );
+			checkNotLoggedIn(); // User is unable to log in......................................
+			
+//			boolean Validate_logIn = driver.findElement(By.xpath("//span[@id='myAccountTrigger']")).isDisplayed();
+// 		    Assert.assertFalse(Validate_logIn );
 		
 		}
 		
 		
 //.....................................................Empty Password................................................................................				
 		
-		@Test(priority=6, description = "Validate error with empty password")
+		@Test(priority=6, description = "Varify error text with empty password and user will be not able to log in")
 		public void validPassTest1() throws InterruptedException {
 			
 			loginPage=new LogInPage(driver);
 			loginPage.login("shoily9192@gmail.com", "");;
 	        String validationError_actual = driver.findElement(By.xpath("//form[1]/div[1]/div[1]/div[2]/span[1]")).getText();
 	        String validationError_expected = "You can't leave this empty.";
-	        Assert.assertTrue(validationError_actual.contains(validationError_expected));
+	        Assert.assertTrue(validationError_actual.contains(validationError_expected)); // check error message is displayed
+	        
+	        checkNotLoggedIn(); // User is unable to log in......................................
 		
 		}
 		
 		
 //.....................................................Password 123...............................................................................			
 		
-		@Test(priority=6, description = "Validate error with password 123")
+		@Test(priority=7, description = "Varify error text with password 123 and user will be not able to log in")
 		public void validPassTest2() throws InterruptedException {
 			
 			loginPage=new LogInPage(driver);
 			loginPage.login("shoily9192@gmail.com", "123");
+			
+//			checkNotLoggedIn(); // User is unable to log in......................................
+			
 			boolean Validate_logIn = driver.findElement(By.xpath("//span[@id='myAccountTrigger']")).isDisplayed();
 			Assert.assertFalse(Validate_logIn );
 		
